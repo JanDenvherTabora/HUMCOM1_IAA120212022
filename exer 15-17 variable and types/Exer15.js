2a3e870672904d198d25c374640abb6912d17f69
JanDenvherTabora <98378734+JanDenvherTabora@users.noreply.github.com>
2022-05-13 22:04:23 +0800

    Add files via upload

---
+++ exer 15-17 variable and types/Exer15.js
@@ -0,0 +1,11 @@
+function displayAssignedValues(){
+    var firstName = document.getElementById("firstName").value = "Tabora Jan Denvher";
+    var age = document.getElementById("age").value = "21";
+    var phoneNumber = document.getElementById("phoneNumber").value = "09105238959";
+    var eMail = document.getElementById("eMail").value = "20139610@s.ubaguio.edu";
+    var mailAddress = document.getElementById("mailAddress").value = "Private magsaysay ave, Baguio City";
+    var program = document.getElementById("program").value = "Computer Science";
+    var idNum = document.getElementById("idNum").value = "20139610";
+    var yearLevel = document.getElementById("yearLevel").value = "1st Year";
+    var section = document.getElementById("section").value = "IAA1";
+}
